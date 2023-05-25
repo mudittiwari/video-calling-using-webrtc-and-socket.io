4918c8e7c56adb427dc4b088958d4afa7685c769
mudit <mudit19.072@gmail.com>
2023-05-25 21:33:04 +0530

    first commit

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
 import React from 'react';
-export const socket = io.connect("http://localhost:5000");
+export const socket = io.connect("https://socket-io-file-sharing.onrender.com");
 export const SocketContext = React.createContext();
